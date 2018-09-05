0a7d9a8fe1928741da916257e98b72758cd646c5
TimothyFell <timothy.fell846@gmail.com>
2018-09-04 21:17:18 -0600

    Player class WIP, still need to validate input

--- lib/ship.rb
+++ lib/ship.rb
@@ -7,23 +7,6 @@ class Ship
   def initialize(size_arg, board_arg)
     @size = size_arg
     @board = board_arg
-    @character_map = {
-      "A" => 2,
-      "B" => 3,
-      "C" => 4,
-      "D" => 5
-    }
-  end
-
-  def convert_user_input(coords)
-    coords_array = coords.split(" ")
-    new_coords_array = coords_array.map do |coord|
-      coord_chars = coord.chars
-      coord_chars[0] = @character_map[coord_chars.first]
-      coord_chars[-1] = coord_chars.last.to_i * 2
-      coord_chars
-    end
-    return new_coords_array
   end
 
   def place(array_indices)
