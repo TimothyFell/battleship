b7a9be3b0465abc040be4f4e5af1252bb87230f0
TimothyFell <timothy.fell846@gmail.com>
2018-09-04 20:22:42 -0600

    Write ship class with minor edits for board and ship tests

--- lib/ship.rb
+++ lib/ship.rb
@@ -2,12 +2,36 @@ require 'pry'
 
 class Ship
 
-  def initialize(size_arg)
+  attr_reader :size
+
+  def initialize(size_arg, board_arg)
     @size = size_arg
+    @board = board_arg
+    @character_map = {
+      "A" => 2,
+      "B" => 3,
+      "C" => 4,
+      "D" => 5
+    }
   end
 
-  def place(coords)
-    
+  def convert_user_input(coords)
+    coords_array = coords.split(" ")
+    new_coords_array = coords_array.map do |coord|
+      coord_chars = coord.chars
+      coord_chars[0] = @character_map[coord_chars.first]
+      coord_chars[-1] = coord_chars.last.to_i * 2
+      coord_chars
+    end
+    return new_coords_array
+  end
+
+  def place(array_indices)
+    array_indices.each do |index_array|
+      row = index_array[0]
+      col = index_array[1]
+      @board.board_array[row][col] = "S"
+    end
   end
 
 end
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -6,38 +6,30 @@ class ShipTest < Minitest::Test
 
   def test_it_exists
     player_board = Board.new("Tim")
-    player_coords = Coordinates.new(player_board)
-    ship_2 = Ship.new(2, player_coords)
+    ship_2 = Ship.new(2, player_board)
 
     assert_instance_of Ship, ship_2
   end
 
   def test_it_has_length
     player_board = Board.new("Tim")
-    player_coords = Coordinates.new(player_board)
-    ship_2 = Ship.new(2, player_coords)
+    ship_2 = Ship.new(2, player_board)
 
     assert_equal 2, ship_2.size
   end
 
   def test_it_can_have_different_lengths
     player_board = Board.new("Tim")
-    player_coords = Coordinates.new(player_board)
-    ship_3 = Ship.new(3, player_coords)
+    ship_3 = Ship.new(3, player_board)
 
     assert_equal 3, ship_3.size
   end
 
   def test_can_be_placed_at_coordinates
-    skip
     player_board = Board.new("Tim")
-    player_coords = Coordinates.new(player_board)
-    ship_2 = Ship.new(2, player_coords)
-    ship_2.place("A1, B1")
-
-    assert_equal true, player_coords.coordinates["A1"].occupied?
-    assert_equal true, player_coords.coordinates["B1"].occupied?
-
+    ship_2 = Ship.new(2, player_board)
+    array_indices = ship_2.convert_user_input("A1 B1")
+    ship_2.place(array_indices)
   end
 
 end
