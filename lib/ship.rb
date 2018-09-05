8034615dea8baf1f5df4096101dcdc3fdae2806a
TimothyFell <timothy.fell846@gmail.com>
2018-09-04 19:56:25 -0600

    Write ship tests

---
+++ lib/ship.rb
@@ -0,0 +1,13 @@
+require 'pry'
+
+class Ship
+
+  def initialize(size_arg)
+    @size = size_arg
+  end
+
+  def place(coords)
+    
+  end
+
+end
---
+++ test/ship_test.rb
@@ -0,0 +1,42 @@
+require './test_helper'
+require './lib/board'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+
+  def test_it_exists
+    player_board = Board.new("Tim")
+    player_coords = Coordinates.new(player_board)
+    ship_2 = Ship.new(2, player_coords)
+
+    assert_instance_of Ship, ship_2
+  end
+
+  def test_it_has_length
+    player_board = Board.new("Tim")
+    player_coords = Coordinates.new(player_board)
+    ship_2 = Ship.new(2, player_coords)
+
+    assert_equal 2, ship_2.size
+  end
+
+  def test_it_can_have_different_lengths
+    player_board = Board.new("Tim")
+    player_coords = Coordinates.new(player_board)
+    ship_3 = Ship.new(3, player_coords)
+
+    assert_equal 3, ship_3.size
+  end
+
+  def test_can_be_placed_at_coordinates
+    player_board = Board.new("Tim")
+    player_coords = Coordinates.new(player_board)
+    ship_2 = Ship.new(2, player_coords)
+    ship_2.place("A1, B1")
+
+    assert_equal true, player_coords.coordinates["A1"].occupied?
+    assert_equal true, player_coords.coordinates["B1"].occupied?
+
+  end
+
+end
