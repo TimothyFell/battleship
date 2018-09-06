9cc58d46a4ca289d8583a097781ffa3b8ae314f7
TimothyFell <timothy.fell846@gmail.com>
2018-09-05 18:23:37 -0600

    Write Space class using TDD

--- lib/player.rb
+++ lib/player.rb
@@ -10,29 +10,35 @@ class Player
   def initialize(name_arg)
     @name = name_arg
     @player_board = Board.new(name_arg)
-    @ship_2 = Ship.new(2, @player_board)
-    @ship_3 = Ship.new(3, @player_board)
+    @ship_2 = Ship.new(2)
+    @ship_3 = Ship.new(3)
 
   end
 
-  def shoot(indices, board)
-    indices.flatten
-    row = indices[0]
-    col = indices[1]
-
-    if board.board_array[row][col] == "H" || board.board_array[row][col] == "M"
-      puts "You already shot there, try again!"
-
-    elsif board.board_array[row][col] == "S"
-      board.board_array[row][col] = "H"
-
-    elsif board.board_array[row][col] == " "
-      board.board_array[row][col] = "M"
-
-    else
-      "That isn't a spot on the board, try again!"
-
-    end
+  def place_ship(ship, coordinates)
+    
   end
+
+  def shoot()
+
+  # def shoot(indices, board)
+  #   indices.flatten
+  #   row = indices[0]
+  #   col = indices[1]
+  #
+  #   if board.board_array[row][col] == "H" || board.board_array[row][col] == "M"
+  #     puts "You already shot there, try again!"
+  #
+  #   elsif board.board_array[row][col] == "S"
+  #     board.board_array[row][col] = "H"
+  #
+  #   elsif board.board_array[row][col] == " "
+  #     board.board_array[row][col] = "M"
+  #
+  #   else
+  #     "That isn't a spot on the board, try again!"
+  #
+  #   end
+  # end
 
 end
